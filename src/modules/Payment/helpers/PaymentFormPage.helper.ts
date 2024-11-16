ea94b67e0a10952cb5a253971df53bc5fa20b7c2
Kali Hernandez <hernandezgomezk122@gmail.com>
2024-11-16 15:00:29 -0600

    separated form schema

--- src/modules/Payment/helpers/PaymentFormPage.helper.ts
+++ src/modules/Payment/helpers/PaymentFormPage.helper.ts
@@ -1,57 +1,16 @@
-import * as yup from "yup";
-import { InitialFormModal, PaymentFormModel } from "@payment/utils/paymentForm.model";
+import {
+  InitialFormModal,
+  PaymentFormModel,
+} from "@payment/utils/paymentForm.model";
 import { useMemo, useState } from "react";
 import { FormikHelpers } from "formik";
-import { TypeDocumentList } from "@payment/utils/const";
+import { formSchema } from "@payment/utils/form.schema";
 
 const PaymentFormPageHelper = () => {
   const [visibleAlert, setVisibleAlert] = useState(false);
   const [existQuery, setExistQuery] = useState(false);
 
-  const validationSchema = useMemo(
-    () =>
-      yup.object({
-        targetNumber: yup
-          .string()
-          .required()
-          .test(
-            "no-spaces",
-            "Credit card number must not contain spaces",
-            (value) => {
-              return !value || !/\s/.test(value);
-            }
-          )
-          .transform((value) => value.replace(/\s+/g, ""))
-          .matches(/^[0-9]+$/, "Only numbers are allowed")
-          .min(13)
-          .max(19),
-        currencyId: yup.number().required(),
-
-        docTypeId: yup.number().required(),
-        documentValue: yup
-          .string()
-          .when("docTypeId", (docTypeId: any, schema) => {
-            const selectedDoc = TypeDocumentList.find(
-              (i) => i.value == docTypeId
-            );
-            if (!selectedDoc) return schema.required();
-
-            return schema
-              .matches(selectedDoc.data.regex, selectedDoc.data.errorMessage)
-              .required();
-          }),
-        firstName: yup.string().required(),
-        lastName: yup.string().required(),
-
-        valuePay: yup.lazy((_) => {
-          console.log(existQuery);
-          return existQuery
-            ? yup.number().required()
-            : yup.number().notRequired();
-        }),
-      }),
-    [existQuery]
-  );
+  const validationSchema = useMemo(() => formSchema(existQuery), [existQuery]);
 
   const onSubmit = async (
     values: PaymentFormModel,
---
+++ src/modules/Payment/utils/form.schema.ts
@@ -0,0 +1,39 @@
+import * as yup from "yup";
+import { TypeDocumentList } from "@payment/utils/const";
+
+export function formSchema(existQuery: boolean) {
+  return yup.object({
+    targetNumber: yup
+      .string()
+      .required()
+      .test(
+        "no-spaces",
+        "Credit card number must not contain spaces",
+        (value) => {
+          return !value || !/\s/.test(value);
+        }
+      )
+      .transform((value) => value.replace(/\s+/g, ""))
+      .matches(/^[0-9]+$/, "Only numbers are allowed")
+      .min(13)
+      .max(19),
+    currencyId: yup.number().required(),
+
+    docTypeId: yup.number().required(),
+    documentValue: yup.string().when("docTypeId", (docTypeId: any, schema) => {
+      const selectedDoc = TypeDocumentList.find((i) => i.value == docTypeId);
+      if (!selectedDoc) return schema.required();
+
+      return schema
+        .matches(selectedDoc.data.regex, selectedDoc.data.errorMessage)
+        .required();
+    }),
+    firstName: yup.string().required(),
+    lastName: yup.string().required(),
+
+    valuePay: yup.lazy((_) => {
+      console.log(existQuery);
+      return existQuery ? yup.number().required() : yup.number().notRequired();
+    }),
+  });
+}
